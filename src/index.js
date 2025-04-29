be62ca382678b61e01ce40a8daa1417e3566cee5
Ichiro Yamasaki <ichiroy@Ichiros-MacBook-Air.local>
2025-04-28 18:26:31 -0700

    Configure for custom domain ichiroy.com with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
@@ -9,14 +9,14 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/blogs" element={<Blogs />} />
       <Route path="/blogs/:id" element={<BlogPost />} />
       <Route path="/connect" element={<Connect />} />
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
